0d64e2d14a4772fb53764d95b5c4f9a2cc404d7e
VitorVitima <yukkiegg@gmail.com>
2023-05-09 17:47:25 -0300

    pequenos ajuster

--- app/cards/[types]/[pokemons]/page.js
+++ app/cards/[types]/[pokemons]/page.js
@@ -8,6 +8,7 @@ async function getData(url) {
 }
 async function Pokemon({ params }) {
     const pokemon = await getData(`https://pokeapi.co/api/v2/pokemon/${params.pokemons}`)
+    const pokemonSpecie = await getData(pokemon.species.url)
     return (
         <div className={Style.conteinerAll}>
             <div>
@@ -18,12 +19,15 @@ async function Pokemon({ params }) {
                 <img className={Style.imgPoke} src={pokemon.sprites.other['official-artwork'].front_default}></img>
             </div>
             <div className={Style.conteinerDados}>
-                <div>
+                <div id={Style.statsConteiner}>
                     <span>Stats:</span>
                     <ul>
-                        <li>Weight: {pokemon.weight}g</li>
-                        <li>Height: {pokemon.height}0cm</li>
-                        <li>Hp: {pokemon.stats[0].base_stat}</li>
+                        <li>Weight: <span>{pokemon.weight}g</span></li>
+                        <li>Height: <span>{pokemon.height}0cm</span></li>
+                        <li>Hp: <span>{pokemon.stats[0].base_stat}</span></li>
+                        <li>Defense: <span>{pokemon.stats[2].base_stat}</span></li>
+                        <li>Attack: <span>{pokemon.stats[1].base_stat}</span></li>
+                        <li>Speed: <span>{pokemon.stats[5].base_stat}</span></li>
                     </ul>
                 </div>
                 <div>
@@ -47,7 +51,7 @@ async function Pokemon({ params }) {
                 <div>
                     <span>Type:</span>
                     <ul>
-                        <li>
+                        <li id={Style.typeLink} style={{backgroundColor: `${pokemonSpecie.color.name}`}}>
                             <Link href={`./cards/${pokemon.types[0].type.name}_1`}>
                                 {pokemon.types[0].type.name}
                             </Link>
---
+++ app/loading.js
@@ -0,0 +1,13 @@
+import './loading.css'
+export default function Loading(){
+    return(
+        <div id='conteinerLoad'>
+            <div id="load">
+                <div id='l1'></div>
+                <div id='l2'></div>
+                <div id='l3'></div>
+                <div id='l4'></div>
+            </div>        
+        </div>
+    )
+}
